3ad1a03e59d82ec4beab956bf2ca38f82a31a61e
Nathan Downer <nathandowner123@gmail.com>
2021-08-04 12:05:03 -0500

    add explore nearby seciton

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
 import Head from 'next/head';
+import Banner from '../components/Banner';
 import Header from '../components/Header';
+import SmallCard from '../components/SmallCard';
 
-export default function Home() {
+export default function Home({ exploreData }) {
   return (
     <div className="">
       <Head>
@@ -9,8 +11,38 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
+      <Banner />
 
-      <h1>Let's build Airbnb</h1>
+      <main className="max-w-7xl mx-auto px-8 sm:px-16">
+        <section>
+          <h2 className="text-4xl font-semibold pb-5 pt-6">Explore Nearby</h2>
+
+          {/* Pull data from a server - API endpoints */}
+          {/* '?' is called optional chcaining incaase the data is not avaialable */}
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+            {exploreData?.map(({ img, distance, location }, idx) => (
+              <SmallCard
+                key={idx}
+                img={img}
+                distance={distance}
+                location={location}
+              />
+            ))}
+          </div>
+        </section>
+      </main>
     </div>
   );
+}
+
+export async function getStaticProps() {
+  const exploreData = await fetch('https://links.papareact.com/pyp').then(
+    (res) => res.json()
+  );
+
+  return {
+    props: {
+      exploreData,
+    },
+  };
 }
