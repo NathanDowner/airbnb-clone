2fe9083a3603909cf8f9275b27cf750c6667df52
Nathan Downer <nathandowner123@gmail.com>
2021-08-05 12:07:50 -0500

    tweak padding

--- components/Banner.js
+++ components/Banner.js
@@ -9,11 +9,11 @@ const Banner = () => {
         objectFit="cover"
         className="object-left xs:object-left-center md:object-center"
       />
-      <div className="absolute top-1/3 left-5 md:left-20 lg:left-40">
+      <div className="absolute top-1/3 left-5 md:left-10 lg:left-40">
         <p className="text-5xl xl:text-6xl font-bold  text-white">
           Olympian & <br /> Paralympian <br /> Online <br /> Experiences
         </p>
-        <button className="text-black bg-white px-6 py-2 rounded-xl shadow-md mt-3 font-bold hover:shadow-xl active:scale-90 transition">
+        <button className="text-black bg-white px-6 py-2 rounded-xl shadow-md mt-6 font-bold hover:shadow-xl active:scale-90 transition">
           I'm flexible
         </button>
       </div>
