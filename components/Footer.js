1468440ad222c0ac1280f05528d764c0cb5be67e
Nathan Downer <nathandowner123@gmail.com>
2021-08-06 01:14:57 -0500

    update footer responsiveness

--- components/Footer.js
+++ components/Footer.js
@@ -43,7 +43,7 @@ const footerData = [
 
 const Footer = () => {
   return (
-    <footer className="grid grid-cols-1 md:grid-cols-4 gap-y-10 px-32 py-14 bg-gray-100 text-gray-600">
+    <footer className="grid grid-cols-1  sm:grid-cols-2 md:grid-cols-4 gap-y-10 px-32 py-14 bg-gray-100 text-gray-600">
       {footerData.map(({ heading, links }, idx) => (
         <div key={idx} className="space-y-4 text-xs text-gray-800">
           <h5 className="font-bold">{heading}</h5>
