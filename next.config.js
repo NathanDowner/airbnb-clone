9b0b1702c29144cff4f881c7a644b9e2948446ae
Nathan Downer <nathandowner123@gmail.com>
2021-08-07 23:27:26 -0500

    add env variable to next config file

--- next.config.js
+++ next.config.js
@@ -3,4 +3,8 @@ module.exports = {
   images: {
     domains: ['links.papareact.com', 'a0.muscache.com'],
   },
+  env: {
+    mapbox_key:
+      'pk.eyJ1IjoibmF0aGFuZG93bmVyIiwiYSI6ImNrczB0dzRtczFuZm4ybmszamtsdXRwbWkifQ.sRFP2PrskRDWFqtbowpRNA',
+  },
 };
