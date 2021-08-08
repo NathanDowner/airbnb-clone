c0dc8cf6245495d99e526393ba88a9a22923f430
Nathan Downer <nathandowner123@gmail.com>
2021-08-07 23:28:22 -0500

    add search icon btn for small screens

--- components/Header.js
+++ components/Header.js
@@ -74,15 +74,18 @@ const Header = ({ transparent, dynamic, placeholder }) => {
       </div>
 
       {/* Middle */}
-      <div className="flex items-center border-2 md:shadow-sm rounded-full py-2 bg-white">
-        <input
-          input={searchInput}
-          onChange={(e) => setSearchInput(e.target.value)}
-          className="px-5 bg-transparent outline-none flex-grow text-xs sm:text-lg md:pr-0"
-          type="text"
-          placeholder={placeholder ?? 'Start your search'}
-        />
-        <SearchIcon className="hidden md:inline-flex md:mx-2 h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer" />
+      <div className="flex items-center">
+        <SearchIcon className="sm:hidden h-6 mr-4 text-white cursor-pointer ml-auto inline-block bg-red-400 flex-shrink-0 p-1 rounded-full" />
+        <div className="hidden flex-grow flex-shrink sm:flex items-center border-2 md:shadow-sm rounded-full py-2 bg-white">
+          <input
+            input={searchInput}
+            onChange={(e) => setSearchInput(e.target.value)}
+            className="px-5 bg-transparent outline-none flex-grow text-xs sm:text-lg md:pr-0"
+            type="text"
+            placeholder={placeholder ?? 'Start your search'}
+          />
+          <SearchIcon className="hidden md:inline-flex md:mx-2 h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer" />
+        </div>
       </div>
 
       {/* Right */}
@@ -94,7 +97,7 @@ const Header = ({ transparent, dynamic, placeholder }) => {
         <p className="hidden md:inline cursor-pointer ml-2 nav-item-hover ">
           Become a host
         </p>
-        <GlobeAltIcon className="h-6 cursor-pointer" />
+        <GlobeAltIcon className="h-6 cursor-pointer flex-shrink-0" />
         <AuthMenu />
       </div>
       {Boolean(searchInput) && (
